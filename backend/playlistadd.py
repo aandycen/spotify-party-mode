1f015e00d3ae190ea1c102ea1e1b3c082d545165
victorsch <victor.schiavo52@yahoo.com>
2018-09-15 00:57:16 -0600

    Search and Playlistadd

--- backend/playlistadd.py
+++ backend/playlistadd.py
@@ -1,36 +1,22 @@
 import pprint
 import sys
 
+from search import search
 from search import *
 import spotipy
 import spotipy.util as util
 
 scope = 'playlist-modify-public'
-
-if len(sys.argv) > 3:
-	username = sys.argv[1]
-	playlist_id = sys.argv[2]
-	track_ids = sys.argv[3:]
-else:
-	print("Invalid parameters!")
-	sys.exit()
-
+username = "victorsch52"
 token = util.prompt_for_user_token(username, scope)
 
-
-
-if token:
+def addsong(username, song, playlist_id):
+	track_id = search(song).split()
+	print(track_id)
 	sp = spotipy.Spotify(auth=token)
 	sp.trace = False
-	results = sp.user_playlist_add_tracks(username, playlist_id, track_ids)	
+	results = sp.user_playlist_add_tracks(username, playlist_id, track_id)
 	print(results)
-	print("Tracks added successfully")
-else:
-	print("Can't get token for ", username)
 
 
-def addsong(artist, song):
-	track_id = search.search(artist, song)
-	sp = spotipy.Spotify(auth=token)
-        sp.trace = False
-        results = sp.user_playlist_add_tracks(username, playlist_id, track_id)
+
